88fe59c23c2cfb248211f20b57c18645a320c3cc
rawbytz <rawbytz@gmail.com>
2019-12-10 18:45:21 -0700

    First commit

---
+++ tagIndex.js
@@ -0,0 +1,26 @@
+// [] Edit separator for Blogger
+(function tagIndex_3_4(separator ='"_separator_"') {
+  function toastMsg(str, sec, err) {
+    WF.showMessage(str, err);
+    setTimeout(WF.hideMessage, (sec || 2) * 1000);
+  }
+  function getDescendantTags(item) {
+    const tagCounts = item.isMainDocumentRoot() ? getRootDescendantTagCounts() : item.getTagManager().descendantTagCounts;
+    const tagList = tagCounts ? tagCounts.getTagList() : [];
+    return tagList.map(Tag => Tag.tag)
+  }
+  function newTopBullet(str) {
+    WF.editGroup(() => {
+      WF.createItem(WF.currentItem(), 0);
+      WF.setItemName(WF.focusedItem(), htmlEscapeTextForContent(str));
+    });
+  }
+  const canCreateChild = item => !item.isReadOnly() || item.isMainDocumentRoot() || (item.isAddedSubtreePlaceholder() && !item.data.added_subtree.isReadOnly());
+  const sep = separator || " ";
+  const current = WF.currentItem();
+  const tagNames = getDescendantTags(current);
+  if (tagNames.length === 0) return void toastMsg("No tags found.", 2, true);
+  if (!canCreateChild(current)) return void toastMsg("Read-Only. Cannot create bullet", 5, true);
+  tagNames.sort((a, b) => a.localeCompare(b));
+  newTopBullet(tagNames.join(sep));
+})();
